7d49e6cc92d4cc988cec0d9d222c62fe1428a35b
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-09-03 13:08:07 -0700

    Added code files and test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative '../lib/hotel_system'
+require_relative '../lib/room'
+require_relative '../lib/reservation'
