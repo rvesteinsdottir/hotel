519b620465e4d025059d5479ea0e0509739053a0
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-09-05 16:56:30 -0700

    Commit before date range reversal

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,24 +5,13 @@ module Hotel
     attr_reader :cost, :date_range, :room_id
     
     def initialize(date_range, room_id)
-      raise ArgumentError unless date_range.first.class == Date && date_range.last.class == Date
-      
-      #no longer needed
-      #raise ArgumentError unless (date_range.last - date_range.first) > 0
-      
       @date_range = date_range
+      
+      raise ArgumentError unless date_range.first.class == Date && date_range.last.class == Date
       
       @cost = ((date_range.last - date_range.first) * 200).to_f
       
       @room_id = room_id
     end
-    
-    # def create_date_range(start_date, end_date)
-    #   res_length = (end_date - start_date).to_i
-    #   res_length.times do |i|
-    #     @date_range << (start_date + i)
-    #   end
-    # end
-    
   end
 end
